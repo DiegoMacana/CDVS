2aed6ed449f26c2d7d9d3e6ef9fad948405fa605
Nataorjuela <nataorjuela62@gmail.com>
2021-08-25 12:02:01 -0500

    correcciòn app java

--- LAB02/Patterns/src/main/java/edu/eci/cvds/patterns/App.java
+++ LAB02/Patterns/src/main/java/edu/eci/cvds/patterns/App.java
@@ -1,13 +1,18 @@
+  
 package edu.eci.cvds.patterns;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App{
+    public static void main(String[] args){
+        if(String.join("",args)==""){
+            System.out.println("Hello World!");
+        }
+        else{
+            System.out.println("Hello "+String.join(" ",args)+"!");
+        }
     }
 }
+
